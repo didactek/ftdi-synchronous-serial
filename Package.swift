24a0939a79ae03a2af06ff8dccf82fe48943c1bd
Kit Transue <src@kittransue.net>
2020-07-31 11:08:02 -0700

    Add example consumer

--- Package.swift
+++ Package.swift
@@ -7,6 +7,9 @@ let package = Package(
     name: "ftdi-synchronous-serial",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .executable(
+            name: "example",
+            targets: ["example"]),
         .library(
             name: "ftdi-synchronous-serial",
             targets: ["ftdi-synchronous-serial"]),
@@ -18,6 +21,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "example",
+            dependencies: ["ftdi-synchronous-serial"]),
         .target(
             name: "ftdi-synchronous-serial",
             dependencies: []),
