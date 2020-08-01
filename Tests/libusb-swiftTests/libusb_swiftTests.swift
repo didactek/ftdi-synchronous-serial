b806e486733f0701abe53417dade896a96527123
Kit Transue <src@kittransue.net>
2020-08-01 09:50:41 -0700

    Remove placeholder library API. Adapt example accordingly. Remove placeholder exercise in tests.

--- Tests/libusb-swiftTests/libusb_swiftTests.swift
+++ Tests/libusb-swiftTests/libusb_swiftTests.swift
@@ -6,7 +6,7 @@ final class libusb_swiftTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(libusb_swift().text, "Hello, World!")
+//        XCTAssertEqual(libusb_swift().text, "Hello, World!")
     }
 
     static var allTests = [
