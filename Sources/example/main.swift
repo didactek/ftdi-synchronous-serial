bcef0cf9ebe8776a607532a80a64e7799d5e0477
Kit Transue <ktransue@acm.org>
2020-08-06 08:27:27 -0700

    Try to get endpoint from configuration

--- Sources/example/main.swift
+++ Sources/example/main.swift
@@ -19,6 +19,6 @@ defer {
 
 do { // hoping block scope triggers FtdiSPI.deinit
     let bus = try! FtdiSPI(speedHz: 10_000_000)
-    let data = Data(repeating: 0xff, count: 10)
+    let data = Data(repeating: 0x55, count: 10)  // 85 decimal
     bus.write(data: data, count: data.count)
 }
