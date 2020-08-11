ab5729f932b821d803dfb9691aff80505d6aa590
Kit Transue <ktransue@acm.org>
2020-08-10 18:25:59 -0700

    Add swift-log.

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -32,7 +32,7 @@ let package = Package(
             dependencies: ["LibUSB", "CInterop"]),
         .target(
             name: "LibUSB",
-            dependencies: ["CLibUSB"]),
+            dependencies: ["CLibUSB", .product(name: "Logging", package: "swift-log")]),
         .target(
             name: "CInterop",
             dependencies: []),
