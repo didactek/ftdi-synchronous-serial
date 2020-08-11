2e3fc7096c4a9720b3758d745fe2cbcbcd0f58a1
Kit Transue <ktransue@acm.org>
2020-08-11 08:37:56 -0700

    Add wIndex to controlTransferOut. Speculate on semantics for D2XX.

--- Sources/LibUSB/USBDevice.swift
+++ Sources/LibUSB/USBDevice.swift
@@ -25,7 +25,6 @@ public class USBDevice {
     }
     
     var handle: OpaquePointer? = nil
-    var wIndex: UInt16 = 1  // FIXME
     var usbWriteTimeout: UInt32 = 5000  // FIXME
     let writeEndpoint: UInt8
     let readEndpoint: UInt8
@@ -120,7 +119,7 @@ public class USBDevice {
     }
     
 
-    func controlTransferOut(bRequest: UInt8, value: UInt16, data: Data? = nil) {
+    func controlTransferOut(bRequest: UInt8, value: UInt16, wIndex: UInt16, data: Data? = nil) {
         let requestType = controlRequest(type: .vendor, direction: .hostToDevice, recipient: .device)
         
         var dataCopy = Array(data ?? Data())
@@ -137,6 +136,13 @@ public class USBDevice {
     }
     
     func controlTransfer(requestType: BMRequestType, bRequest: UInt8, wValue: UInt16, wIndex: UInt16, data: UnsafeMutablePointer<UInt8>!, wLength: UInt16, timeout: UInt32) -> Int32 {
+        // USB 2.0 9.3.4: wIndex
+        // some intpretations (high bits 0):
+        //   as endpoint (direction:1/0:3/endpoint:4)
+        //   as interface (interface number)
+        // semantics for ControlRequestType.standard requests are defined in
+        // Table 9.4 Standard Device Requests
+        // ControlRequestType.vendor semantics may vary.
         libusb_control_transfer(handle, requestType, bRequest, wValue, wIndex, data, wLength, timeout)
     }
     
