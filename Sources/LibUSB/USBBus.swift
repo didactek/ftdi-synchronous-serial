10773f6f923dd304d13daae0ea6a9ea5fece9540
Kit Transue <ktransue@acm.org>
2020-08-12 11:00:33 -0700

    Tighten memory management somewhat.

--- Sources/LibUSB/USBBus.swift
+++ Sources/LibUSB/USBBus.swift
@@ -8,21 +8,28 @@
 import Foundation
 import CLibUSB
 
+
 // FIXME: "system"? Could this manage the close() call automatically?
 public class USBBus {
-     static let ctx: OpaquePointer? = nil // for sharing libusb contexts, init, etc.
+    static let ctx: OpaquePointer? = nil // for sharing libusb contexts, init, etc.
     
+    // FIXME: since this is public, should it return a type with clearer semantics?
     public static func findDevice() -> OpaquePointer {
         // scan for devices:
         var devices: UnsafeMutablePointer<OpaquePointer?>? = nil
         let deviceCount = libusb_get_device_list(Self.ctx, &devices)
+        defer {
+            libusb_free_device_list(devices, 1)
+        }
         guard deviceCount > 0 else {
             fatalError("no USB devices found")
         }
         logger.debug("found \(deviceCount) devices")
         
         // find the device
-        // FIXME: be more precise than this!
+        // FIXME: be more precise than this! Look at all the entries; libusb
+        // says libusb_open_device_with_vid_pid is a crutch and that enumeration
+        // is the right way to go....
         let device = devices![0]
         
         #if true // optional: this is just "we found something!" reassurance
@@ -30,7 +37,8 @@ public class USBBus {
         let _ = libusb_get_device_descriptor(device, &descriptor)
         logger.debug("vendor: \(String(descriptor.idVendor, radix: 16))")
         logger.debug("product: \(String(descriptor.idProduct, radix: 16))")
-
+        #endif
+        
         #if false  // FIXME: do string lookup
         // get the serial number:
         // need a lang descriptor
@@ -41,7 +49,8 @@ public class USBBus {
         // some kind of get_description call here
         logger.debug("device has \(descriptor.bNumConfigurations) configurations")
         #endif
-        #endif
+
+        libusb_ref_device(device!)  // protect this one from deferred libusb_free_device_list
         return device!
     }
 
