ed280a4785e262261c5fbbd2ced264600d9bc0d9
Kit Transue <ktransue@acm.org>
2020-09-04 20:22:40 -0700

    Move SPI Mode Specification to its own file.

---
+++ Sources/FTDI/SPIModeSpec.swift
@@ -0,0 +1,36 @@
+//
+//  SPIModeSpec.swift
+//  
+//
+//  Created by Kit Transue on 2020-09-04.
+//
+
+import Foundation
+
+/// SPI Modes and their specifications.
+enum SPIModeSpec {
+    case mode0
+    #if false  // remainder not currently implemented
+    case mode1
+    case mode2
+    case mode3
+    #endif
+
+    /// The edge when writing data must be valid.
+    var writeWindow: DataWindow {
+        switch self {
+        case .mode0:
+            return .fallingEdge
+        }
+    }
+    
+    /// clock and dataOut values for an idle bus.
+    /// (as a bit field)
+    // FIXME: how to make this implementation-independent so this can be moved to a specification-only file?
+    var busAtIdle: UInt8 {
+        switch self {
+        case .mode0:
+            return 0
+        }
+    }
+}
