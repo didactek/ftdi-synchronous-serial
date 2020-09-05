f62725276679ea42042930202cee4352dea42e98
Kit Transue <src@kittransue.net>
2020-09-04 19:41:14 -0700

    Document FtdiI2CDevice.

--- Sources/FTDI/FtdiI2CDevice.swift
+++ Sources/FTDI/FtdiI2CDevice.swift
@@ -9,27 +9,39 @@
 
 import Foundation
 
+/// Manage conversations with a specific device at an I2C address, adding necessary start/stop blocks
+/// to sustain and complete a conversation, including the two-start/one-stop `writeAndRead` operation.
 public class FtdiI2CDevice {
     let address: UInt8
     let bus: FtdiI2C
 
-    // FIXME: throw if the address isn't found on the bus?
-    public init(bus: FtdiI2C, nodeAddress: Int) throws {
-        self.bus = bus
+    /// - Parameter busHost: Host from which I2C clock/conversations originate.
+    /// - Parameter nodeAddress: I2C address of the target device on the bus.
+    public init(busHost: FtdiI2C, nodeAddress: Int) throws {
+        self.bus = busHost
         self.address = UInt8(nodeAddress)
     }
 
+    /// Write bytes to the device, preceded by a 'start' and followed by a 'stop'.
     public func write(data: Data) {
         bus.write(address: address, data: data)
         bus.sendStop()
     }
 
+    /// Read bytes from the device, preceded by a 'start' and followed by a 'stop'.
+    /// - Parameter count: Number of bytes to read.
+    /// - Note: For devices that adopt a named register, address, or command idioms, use
+    /// `writeAndRead` to send the name/address/command and read the response.
     public func read(count: Int) -> Data {
         let data = bus.read(address: address, count: count)
         bus.sendStop()
         return data
     }
 
+    /// Write bytes to the device, read a response, and then send a 'stop' to end the conversation.
+    /// - Parameter sendFrom: Bytes to send in the first conversation fragment.
+    /// - Parameter receiveCount: Number of bytes to read in the second conversation fragment.
+    /// - Returns: Bytes read.
     public func writeAndRead(sendFrom: Data, receiveCount: Int) -> Data {
         bus.write(address: address, data: sendFrom)
         let data = bus.read(address: address, count: receiveCount)
